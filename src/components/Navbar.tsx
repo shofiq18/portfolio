ae91efdf74a5025fd824c9ad39b2ea3db552eb04
shofiq18 <shafiqul.islam25021998@gmail.com>
2026-03-29 21:42:54 +0600

    fix gsap

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import HoverLinks from "./HoverLinks";
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
-import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
 import "./styles/Navbar.css";
 import { MdArrowOutward } from "react-icons/md";
 
